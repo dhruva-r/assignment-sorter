128165414452a329818c29ae48d34389b307bec7
kareemassad <kareemassad5@gmail.com>
2019-02-16 17:42:59 -0500

    Added info about the project.

---
+++ test.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+from tkinter import *
+
+from tkinter import messagebox
+
+top = Tk()
+top.geometry("200x200")
+def hello():
+   messagebox.showinfo("Say Hello", "Hello World")
+
+B1 = Button(top, text = "Say Hello", command = hello)
+B1.place(x = 35,y = 70)
+
+top.mainloop()
