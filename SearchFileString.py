761d6c6d188c39aa53150cf710542b9e07b2ecd3
Dhruva Rajkumar <dhruva.rajkumar@uwaterloo.ca>
2019-02-16 15:03:56 -0500

    SearchFileString

--- SearchFileString.py
+++ SearchFileString.py
@@ -1,18 +1,24 @@
 import tkinter
 import tkinter.filedialog
+import ntpath
 
 filename = tkinter.filedialog.askopenfilenames()
 print(filename)
 print(len(filename))
+tail = filename[0]
+print(tail)
+head, tail = ntpath.split(filename[0])
+print(head)
+print(tail)
+# this function just checks the filename for the keyword, returns true if it is there, false if not
 
-#this function just checks the filename for the keyword, returns true if it is there, false if not
 
 def search(fname, kword):
     if fname.find(kword) != -1:
         return True
     return False
 
-def returnFilename(fPath):
 
+def returnfilename(fPath):
 
     return ""
