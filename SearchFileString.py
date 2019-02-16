706255c55ef9606b67cb06b6c2a7298f7587001f
Matthew <matthewkwsiu@yahoo.ca>
2019-02-16 14:43:37 -0500

    This is the two functions

--- SearchFileString.py
+++ SearchFileString.py
@@ -4,3 +4,15 @@ import tkinter.filedialog
 filename = tkinter.filedialog.askopenfilenames()
 print(filename)
 print(len(filename))
+
+#this function just checks the filename for the keyword, returns true if it is there, false if not
+
+def search(fname, kword):
+    if fname.find(kword) != -1:
+        return True
+    return False
+
+def returnFilename(fPath):
+
+
+    return ""
