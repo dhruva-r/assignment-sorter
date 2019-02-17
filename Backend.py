1183e793dcd7b216bd6ca28f3f2dd692c6f60a94
Dhruva Rajkumar <dhruva.rajkumar@uwaterloo.ca>
2019-02-16 23:05:28 -0500

    Combined backend into one file, rename update next.

--- Backend.py
+++ Backend.py
@@ -17,7 +17,7 @@ def returnfilename(filepath):
     return tail
 
 
-def sortfolder(filenames, keyword, directory):
+def sortfolder(filenames, keyword, dir):
     length = (len(filenames))
     # this keyword will be set based on the input of the user on what to search
 
@@ -25,33 +25,33 @@ def sortfolder(filenames, keyword, directory):
         filepath = filenames[x]
         filename = returnfilename(filepath)
         if search(filename, keyword):
-            create_folder(directory, keyword)
-            print(directory)
-            trans(directory, filepath, keyword)
+            create_folder(dir, keyword)
+            print(dir)
+            trans(dir, filepath, keyword)
 
 
-def create_folder(directory, key):
+def create_folder(dire, key):
     # This function checks if a dir exists if not, throws error and prints to console, if it does creates folder
     try:
-        if not os.path.exists(directory + "/" + key):
-            os.makedirs(directory + "/" + key)
+        if not os.path.exists(dire + "/" + key):
+            os.makedirs(dire + "/" + key)
         else:
             print("Folder Already Exists")
     except OSError:
-        print('Error: Creating directory. ' + directory)
+        print('Error: Creating directory. ' + dire)
 
 # Creates a folder in the current directory called data
 
 
-def trans(directory, path, key):
+def trans(direc, path, key):
     # this function transports the files by renaming the file
     print(path)
-    print(directory + key + "/" + returnfilename(path))
-    os.rename(path, directory + "/" + key + "/" + returnfilename(path))
+    print(direc + key + "/" + returnfilename(path))
+    os.rename(path, direc + "/" + key + "/" + returnfilename(path))
 
 
-i = 0
 keywords = []
+directory = "C:/Users/dhruv/Documents"
 
 
 def grab_keywords(k):
@@ -59,11 +59,15 @@ def grab_keywords(k):
     keywords.append(k)
 
 
+def set_directory(s):
+
+    directory = s
+
+
 def main():
     # this returns a tuple based on the selected files of the user
     filenames = tkinter.filedialog.askopenfilenames()
     keywords.append(".")
-    directory = "C:/Users/dhruv/Documents"
     for x in range(len(keywords)):
         keyword = keywords[x]
         sortfolder(filenames, keyword, directory)
